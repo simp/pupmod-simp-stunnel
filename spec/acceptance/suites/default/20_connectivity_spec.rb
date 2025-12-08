c840c8044fe56137fa66ece3c2ae63ab0979dac6
Steven Pritchard <steve@sicura.us>
2025-12-08 12:18:14 -0600

    Clean up for rubocop (#119)
    * Clean up for rubocop
    * Update gems and additional cleanup
    
    ---------
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/acceptance/suites/default/20_connectivity_spec.rb
+++ spec/acceptance/suites/default/20_connectivity_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper_acceptance'
 test_name 'instance connectivity'
 
 describe 'instance connectivity' do
-
   if hosts.count < 2
     it 'only runs with more than one host' do
       skip('You need at least two hosts in your nodeset to run this test')
     end
   else
-    context 'system prep' do
+    before(:all) do
       hosts.each do |host|
         install_package(host, 'nc')
       end
@@ -18,8 +17,7 @@ describe 'instance connectivity' do
     context 'set up a bi-directional connection set' do
       hosts.each do |server|
         hosts.each do |client|
-
-          server_fqdn = fact_on(server, 'fqdn')
+          server_fqdn = fact_on(server, 'networking.fqdn')
 
           hieradata = {
             'iptables::ports'            => { 22 => { 'proto' => 'tcp', 'trusted_nets' => ['ALL'] } },
@@ -28,49 +26,49 @@ describe 'instance connectivity' do
             'simp_options::firewall'     => true,
             'simp_options::pki'          => true,
             'simp_options::pki::source'  => '/etc/pki/simp-testing/pki/',
-            'simp_options::trusted_nets' => [client.ip]
+            'simp_options::trusted_nets' => [client.ip],
           }
 
-          manifest = <<-EOF
+          manifest = <<~EOF
             stunnel::instance { 'mysvc':
               client  => false,
               connect => [1234],
-              accept  => 12345
+              accept  => 12345,
             }
 
             stunnel::instance { 'mysvc-client':
               client  => true,
               connect => ['#{server_fqdn}:12345'],
               accept  => 1235,
-              require => Stunnel::Instance['mysvc']
+              require => Stunnel::Instance['mysvc'],
             }
           EOF
 
           context "with server #{server} and client #{client}" do
             [server, client].each do |host|
-              it "should clean up #{host}" do
-                on(host, 'pkill -f nc', :accept_all_exit_codes => true)
+              it "cleans up #{host}" do
+                on(host, 'pkill -f nc', accept_all_exit_codes: true)
               end
 
-              it "should apply on #{host} with no errors" do
+              it "applies on #{host} with no errors" do
                 set_hieradata_on(host, hieradata)
                 apply_manifest_on(host, manifest)
               end
 
-              it "should be idempotent on #{host}" do
+              it "is idempotent on #{host}" do
                 apply_manifest_on(host, manifest, catch_changes: true)
               end
             end
 
-            it "should set up netcat to listen on #{server}" do
+            it "sets up netcat to listen on #{server}" do
               on(server, 'nc -k -l 1234 > /tmp/ncout.txt 2>&1 &')
             end
 
-            it "should send successfully from #{client}" do
+            it "sends successfully from #{client}" do
               on(client, %(/bin/echo "#{client.ip}" | nc -w1 localhost 1235))
             end
 
-            it "should be received successfully on #{server}" do
+            it "is received successfully on #{server}" do
               output = on(server, 'tail -n1 /tmp/ncout.txt').stdout.strip
               expect(output).to eq client.ip
             end
--- spec/defines/connection_spec.rb
+++ spec/defines/connection_spec.rb
@@ -4,262 +4,280 @@ describe 'stunnel::connection' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let(:facts){ os_facts }
+        let(:facts) { os_facts }
 
         context 'with default parameters' do
-          let(:title){ 'test_tunnel' }
-          let(:params){{
-            :connect => [2048,2049],
-            :accept  => '10.1.2.3:20490',
-          }}
+          let(:title) { 'test_tunnel' }
+          let(:params) do
+            {
+              connect: [2048, 2049],
+              accept: '10.1.2.3:20490',
+            }
+          end
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_stunnel__instance__reserve_port('20490') }
           it { is_expected.to create_class('stunnel::monolithic') }
           it do
-            expected = <<-EOM
-[test_tunnel]
-connect = 2048
-connect = 2049
-accept = 10.1.2.3:20490
-client = yes
-failover = rr
-key = /etc/pki/simp_apps/stunnel/x509/private/foo.example.com.pem
-cert = /etc/pki/simp_apps/stunnel/x509/public/foo.example.com.pub
-CAfile = /etc/pki/simp_apps/stunnel/x509/cacerts/cacerts.pem
-ciphers = HIGH:-SSLv2
-sslVersion = TLSv1.2
-verify = 2
-delay = no
+            expected = <<~EOM
+              [test_tunnel]
+              connect = 2048
+              connect = 2049
+              accept = 10.1.2.3:20490
+              client = yes
+              failover = rr
+              key = /etc/pki/simp_apps/stunnel/x509/private/foo.example.com.pem
+              cert = /etc/pki/simp_apps/stunnel/x509/public/foo.example.com.pub
+              CAfile = /etc/pki/simp_apps/stunnel/x509/cacerts/cacerts.pem
+              ciphers = HIGH:-SSLv2
+              sslVersion = TLSv1.2
+              verify = 2
+              delay = no
+              renegotiation = yes
+              reset = yes
             EOM
-            expected  += "renegotiation = yes\nreset = yes\n"
             is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(expected)
           end
-
         end
 
         context 'using iptables' do
-          let(:title){ 'nfs' }
-          let(:params){{
-            :client       => false,
-            :connect      => [2049],
-            :accept       => 20490,
-            :trusted_nets => ['any'],
-            :firewall     => true
-          }}
+          let(:title) { 'nfs' }
+          let(:params) do
+            {
+              client: false,
+              connect: [2049],
+              accept: 20_490,
+              trusted_nets: ['any'],
+              firewall: true,
+            }
+          end
+
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_stunnel__instance__reserve_port('20490') }
-          it { is_expected.to create_iptables__listen__tcp_stateful("allow_stunnel_#{title}").with({
-            :trusted_nets => ['any'],
-            :dports       => [params[:accept].to_s.split(':')[-1]]
-            })
+          it {
+            is_expected.to create_iptables__listen__tcp_stateful("allow_stunnel_#{title}").with(
+              trusted_nets: ['any'],
+              dports: [params[:accept].to_s.split(':')[-1]],
+            )
           }
         end
 
         context 'using tcpwrappers' do
-          let(:title){ 'nfs' }
-          let(:params){{
-            :tcpwrappers  => true,
-            :client       => false,
-            :connect      => [2049],
-            :accept       => 20490,
-            :trusted_nets => ['any']
-          }}
+          let(:title) { 'nfs' }
+          let(:params) do
+            {
+              tcpwrappers: true,
+              client: false,
+              connect: [2049],
+              accept: 20_490,
+              trusted_nets: ['any'],
+            }
+          end
+
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_tcpwrappers__allow("allow_stunnel_#{title}").with_pattern(['ALL']) }
         end
 
         context 'setting ocsp options' do
-          let(:title){ 'nfs' }
-          let(:params){{
-            :client       => false,
-            :connect      => [2049],
-            :accept       => 20490,
-            :trusted_nets => ['any'],
-            :ocsp         => 'http://foo.bar.baz',
-            :ocsp_flags   => ['NOCERTS', 'NOCHAIN']
-          }}
+          let(:title) { 'nfs' }
+          let(:params) do
+            {
+              client: false,
+              connect: [2049],
+              accept: 20_490,
+              trusted_nets: ['any'],
+              ocsp: 'http://foo.bar.baz',
+              ocsp_flags: ['NOCERTS', 'NOCHAIN'],
+            }
+          end
+
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(/OCSPFlag = NOCERTS/) }
-          it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(/OCSPFlag = NOCHAIN/) }
+          it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(%r{OCSPFlag = NOCERTS}) }
+          it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(%r{OCSPFlag = NOCHAIN}) }
         end
 
         context 'with other parameters set' do
-          let(:title){ 'test_tunnel' }
-          let(:params){{
-            :connect                 => [2048,2049],
-            :accept                  => '10.1.2.3:20490',
-            :curve                   => 'prime256v1',
-            :local                   => '1.2.3.4',
-            :options                 => ['SSL_OP_NETSCAPE_CHALLENGE_BUG', 'SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS'],
-            :protocol                => 'connect',
-            :protocol_authentication => 'basic',
-            :protocol_host           => '1.2.3.5:2050',
-            :protocol_password       => 'some_password',
-            :protocol_username       => 'some_user',
-            :engine_num              => 1,
-            :exec                    => '/some/exec',
-            :execargs                => ['arg1', 'arg2'],
-            :session_cache_timeout   => 20,
-            :session_cache_size      => 1000,
-            :sni                     => 'test.sni.server',
-            :ssl_version             => 'TLSv1',
-            :stack                   => 1024,
-            :timeout_busy            => 5,
-            :timeout_close           => 10,
-            :timeout_connect         => 15,
-            :timeout_idle            => 20
-          }}
+          let(:title) { 'test_tunnel' }
+          let(:params) do
+            {
+              connect: [2048, 2049],
+              accept: '10.1.2.3:20490',
+              curve: 'prime256v1',
+              local: '1.2.3.4',
+              options: ['SSL_OP_NETSCAPE_CHALLENGE_BUG', 'SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS'],
+              protocol: 'connect',
+              protocol_authentication: 'basic',
+              protocol_host: '1.2.3.5:2050',
+              protocol_password: 'some_password',
+              protocol_username: 'some_user',
+              engine_num: 1,
+              exec: '/some/exec',
+              execargs: ['arg1', 'arg2'],
+              session_cache_timeout: 20,
+              session_cache_size: 1000,
+              sni: 'test.sni.server',
+              ssl_version: 'TLSv1',
+              stack: 1024,
+              timeout_busy: 5,
+              timeout_close: 10,
+              timeout_connect: 15,
+              timeout_idle: 20,
+            }
+          end
 
           it { is_expected.to compile.with_all_deps }
 
-          [ /sni = test.sni.server/,
-            /sessionCacheTimeout = 20/,
-            /sessionCacheSize = 1000/,
-            /renegotiation = yes/,
-            /reset = yes/
-          ].each do |exp_regex|
-            it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(exp_regex) }
-          end
-
-          [ /curve = prime256v1/,
-            /sslVersion = TLSv1/,
-            /options = SSL_OP_NETSCAPE_CHALLENGE_BUG/,
-            /options = SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS/,
-            /local = 1.2.3.4/,
-            /protocol = connect/,
-            /protocolAuthentication = basic/,
-            /protocolHost = 1\.2\.3\.5:2050/,
-            /protocolPassword = some_password/,
-            /protocolUsername = some_user/,
-            /engineNum = 1/,
-            /exec = \/some\/exec/,
-            /execargs = arg1 arg2/,
-            /pty = no/,
-            /retry = no/,
-            /stack = 1024/,
-            /TIMEOUTbusy = 5/,
-            /TIMEOUTclose = 10/,
-            /TIMEOUTconnect = 15/,
-            /TIMEOUTidle = 20/
+          [
+            %r{sni = test.sni.server},
+            %r{sessionCacheTimeout = 20},
+            %r{sessionCacheSize = 1000},
+            %r{renegotiation = yes},
+            %r{reset = yes},
+            %r{curve = prime256v1},
+            %r{sslVersion = TLSv1},
+            %r{options = SSL_OP_NETSCAPE_CHALLENGE_BUG},
+            %r{options = SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS},
+            %r{local = 1.2.3.4},
+            %r{protocol = connect},
+            %r{protocolAuthentication = basic},
+            %r{protocolHost = 1\.2\.3\.5:2050},
+            %r{protocolPassword = some_password},
+            %r{protocolUsername = some_user},
+            %r{engineNum = 1},
+            %r{exec = /some/exec},
+            %r{execargs = arg1 arg2},
+            %r{pty = no},
+            %r{retry = no},
+            %r{stack = 1024},
+            %r{TIMEOUTbusy = 5},
+            %r{TIMEOUTclose = 10},
+            %r{TIMEOUTconnect = 15},
+            %r{TIMEOUTidle = 20},
           ].each do |exp_regex|
             it { is_expected.to create_concat__fragment("stunnel_connection_#{title}").with_content(exp_regex) }
           end
         end
 
         context 'with Hiera overrides' do
-          let(:pre_condition) { <<-EOM
-            stunnel::connection{ 'nfs_client':
-              accept  => '127.0.0.1:2049',
-              connect => ['my.nfs.host:20490']
-            }
+          let(:pre_condition) do
+            <<~EOM
+              stunnel::connection{ 'nfs_client':
+                accept  => '127.0.0.1:2049',
+                connect => ['my.nfs.host:20490'],
+              }
             EOM
-          }
+          end
 
-          let(:title){ 'nfs' }
-          let(:params){{
-            :client       => false,
-            :connect      => [2049],
-            :accept       => 20490,
-            :trusted_nets => ['any'],
-            # Needed to force a recompile in each context
-            :ocsp         => ocsp,
-            :ocsp_flags   => ['NOCERTS']
-          }}
+          let(:title) { 'nfs' }
+          let(:params) do
+            {
+              client: false,
+              connect: [2049],
+              accept: 20_490,
+              trusted_nets: ['any'],
+              # Needed to force a recompile in each context
+              ocsp: ocsp,
+              ocsp_flags: ['NOCERTS'],
+            }
+          end
 
           context 'global override' do
             let(:ocsp) { 'https://global.bar.baz' }
-            let(:hieradata){ 'defined_type_global_override' }
+            let(:hieradata) { 'defined_type_global_override' }
 
             it { is_expected.to compile.with_all_deps }
-            it { is_expected.to create_stunnel__connection(title).with(
-              :app_pki_cacert          => '/some/global/cacerts.pem',
-              :app_pki_cert            => '/some/global/test.pub',
-              :app_pki_crl             => '/some/global/crl',
-              :app_pki_key             => '/some/global/test.pem',
-              :curve                   => 'prime256v1',
-              :delay                   => true,
-              :engine_num              => 1,
-              :exec                    => '/some/exec',
-              :execargs                => ['arg1', 'arg2'],
-              :failover                => 'prio',
-              :firewall                => true,
-              :local                   => '1.2.3.4',
-              :options                 => ['SSL_OP_NETSCAPE_CHALLENGE_BUG' ],
-              :ocsp                    => 'https://global.bar.baz',
-              :ocsp_flags              => [ 'NOCERTS' ],
-              :openssl_cipher_suite    => [ 'AES128-SHA256' ],
-              :protocol                => 'connect',
-              :protocol_authentication => 'basic',
-              :protocol_host           => '1.2.3.5:2050',
-              :protocol_password       => 'some_password',
-              :protocol_username       => 'some_user',
-              :pty                     => true,
-              :renegotiation           => false,
-              :reset                   => false,
-              :retry                   => true,
-              :session_cache_size      => 20,
-              :session_cache_timeout   => 1000,
-              :sni                     => 'global.sni.server',
-              :ssl_version             => 'TLSv1',
-              :stack                   => 1024,
-              :tcpwrappers             => true,
-              :timeout_busy            => 5,
-              :timeout_close           => 10,
-              :timeout_connect         => 15,
-              :timeout_idle            => 20,
-              :trusted_nets            => [ 'any' ],
-              :verify                  => 4,
-            ) }
+            it {
+              is_expected.to create_stunnel__connection(title).with(
+                app_pki_cacert: '/some/global/cacerts.pem',
+                app_pki_cert: '/some/global/test.pub',
+                app_pki_crl: '/some/global/crl',
+                app_pki_key: '/some/global/test.pem',
+                curve: 'prime256v1',
+                delay: true,
+                engine_num: 1,
+                exec: '/some/exec',
+                execargs: ['arg1', 'arg2'],
+                failover: 'prio',
+                firewall: true,
+                local: '1.2.3.4',
+                options: ['SSL_OP_NETSCAPE_CHALLENGE_BUG' ],
+                ocsp: 'https://global.bar.baz',
+                ocsp_flags: [ 'NOCERTS' ],
+                openssl_cipher_suite: [ 'AES128-SHA256' ],
+                protocol: 'connect',
+                protocol_authentication: 'basic',
+                protocol_host: '1.2.3.5:2050',
+                protocol_password: 'some_password',
+                protocol_username: 'some_user',
+                pty: true,
+                renegotiation: false,
+                reset: false,
+                retry: true,
+                session_cache_size: 20,
+                session_cache_timeout: 1000,
+                sni: 'global.sni.server',
+                ssl_version: 'TLSv1',
+                stack: 1024,
+                tcpwrappers: true,
+                timeout_busy: 5,
+                timeout_close: 10,
+                timeout_connect: 15,
+                timeout_idle: 20,
+                trusted_nets: [ 'any' ],
+                verify: 4,
+              )
+            }
 
-            it { is_expected.to create_stunnel__connection("nfs_client").with(
-              :app_pki_cacert          => '/some/global/cacerts.pem',
-              :app_pki_cert            => '/some/global/test.pub',
-              :app_pki_crl             => '/some/global/crl',
-              :app_pki_key             => '/some/global/test.pem',
-              :curve                   => 'prime256v1',
-              :delay                   => true,
-              :engine_num              => 1,
-              :exec                    => '/some/exec',
-              :execargs                => ['arg1', 'arg2'],
-              :failover                => 'prio',
-              :firewall                => true,
-              :local                   => '1.2.3.4',
-              :ocsp                    => 'http://foo.bar.baz',
-              :ocsp_flags              => [ 'NOCHAIN' ],
-              :openssl_cipher_suite    => [ 'AES128-SHA256' ],
-              :options                 => ['SSL_OP_NETSCAPE_CHALLENGE_BUG' ],
-              :protocol                => 'connect',
-              :protocol_authentication => 'basic',
-              :protocol_host           => '1.2.3.5:2050',
-              :protocol_password       => 'some_password',
-              :protocol_username       => 'some_user',
-              :pty                     => true,
-              :renegotiation           => false,
-              :reset                   => false,
-              :retry                   => true,
-              :session_cache_size      => 20,
-              :session_cache_timeout   => 1000,
-              :sni                     => 'global.sni.server',
-              :ssl_version             => 'TLSv1',
-              :stack                   => 1024,
-              :tcpwrappers             => true,
-              :timeout_busy            => 5,
-              :timeout_close           => 10,
-              :timeout_connect         => 15,
-              :timeout_idle            => 20,
-              :trusted_nets            => [ '1.2.3.0/24' ],
-              :verify                  => 4,
-            ) }
+            it {
+              is_expected.to create_stunnel__connection('nfs_client').with(
+                app_pki_cacert: '/some/global/cacerts.pem',
+                app_pki_cert: '/some/global/test.pub',
+                app_pki_crl: '/some/global/crl',
+                app_pki_key: '/some/global/test.pem',
+                curve: 'prime256v1',
+                delay: true,
+                engine_num: 1,
+                exec: '/some/exec',
+                execargs: ['arg1', 'arg2'],
+                failover: 'prio',
+                firewall: true,
+                local: '1.2.3.4',
+                ocsp: 'http://foo.bar.baz',
+                ocsp_flags: [ 'NOCHAIN' ],
+                openssl_cipher_suite: [ 'AES128-SHA256' ],
+                options: ['SSL_OP_NETSCAPE_CHALLENGE_BUG' ],
+                protocol: 'connect',
+                protocol_authentication: 'basic',
+                protocol_host: '1.2.3.5:2050',
+                protocol_password: 'some_password',
+                protocol_username: 'some_user',
+                pty: true,
+                renegotiation: false,
+                reset: false,
+                retry: true,
+                session_cache_size: 20,
+                session_cache_timeout: 1000,
+                sni: 'global.sni.server',
+                ssl_version: 'TLSv1',
+                stack: 1024,
+                tcpwrappers: true,
+                timeout_busy: 5,
+                timeout_close: 10,
+                timeout_connect: 15,
+                timeout_idle: 20,
+                trusted_nets: [ '1.2.3.0/24' ],
+                verify: 4,
+              )
+            }
           end
 
           context 'specific override' do
             let(:ocsp) { 'https://specific.bar.baz' }
-            let(:hieradata){ 'defined_type_specific_override' }
+            let(:hieradata) { 'defined_type_specific_override' }
 
             it { is_expected.to compile.with_all_deps }
             it { is_expected.to create_stunnel__connection(title).with_ssl_version('all') }
-            it { is_expected.to create_stunnel__connection("nfs_client").with_ssl_version('TLSv1') }
+            it { is_expected.to create_stunnel__connection('nfs_client').with_ssl_version('TLSv1') }
           end
         end
       end
